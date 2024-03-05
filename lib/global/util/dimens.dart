543b0fd4a7de8b091bb696a7a68f5fa5323d5d2e
Dushan <67400684+DuzziG@users.noreply.github.com>
2024-03-05 13:58:44 +0100

    feat: Add standard dimensions for elements

---
+++ lib/global/util/dimens.dart
@@ -0,0 +1,41 @@
+// ignore_for_file: constant_identifier_names
+
+import 'package:flutter/widgets.dart';
+
+const BOX_0 = SizedBox.shrink();
+
+const BOX_4 = SizedBox(height: 4.0, width: 4.0);
+
+const BOX_8 = SizedBox(height: 8.0, width: 8.0);
+
+const BOX_16 = SizedBox(height: 16.0, width: 16.0);
+
+const BOX_24 = SizedBox(height: 24.0, width: 24.0);
+
+const BOX_32 = SizedBox(height: 32.0, width: 32.0);
+
+const BOX_64 = SizedBox(height: 64.0, width: 64.0);
+
+const BOX_72 = SizedBox(height: 72.0, width: 72.0);
+
+const INSETS_0 = EdgeInsets.all(0.0);
+
+const INSETS_2 = EdgeInsets.all(2.0);
+
+const INSETS_4 = EdgeInsets.all(4.0);
+
+const INSETS_8 = EdgeInsets.all(8.0);
+
+const INSETS_16 = EdgeInsets.all(16.0);
+
+const INSETS_32 = EdgeInsets.all(32.0);
+
+const INSETS_HORIZONTAL_8 = EdgeInsets.symmetric(horizontal: 8.0);
+
+const INSETS_HORIZONTAL_16 = EdgeInsets.symmetric(horizontal: 16.0);
+
+const INSETS_VERTICAL_8 = EdgeInsets.symmetric(vertical: 8.0);
+
+const INSETS_VERTICAL_16 = EdgeInsets.symmetric(vertical: 16.0);
+
+const CARD_RADIUS = 16.0;
