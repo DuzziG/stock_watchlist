a11cced75cde0067592014bfca34dd1f0982e741
Dushan <67400684+DuzziG@users.noreply.github.com>
2024-03-06 15:01:29 +0100

    chore: Improve texts, themes and dimens

--- lib/global/theme/texts.dart
+++ lib/global/theme/texts.dart
@@ -1,8 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 
-final TextStyle textStyle = GoogleFonts.play();
+final TextStyle textStyle = GoogleFonts.dosis();
 
-final TextTheme textThemeLight = GoogleFonts.playTextTheme(ThemeData.light().textTheme);
+final TextTheme textThemeLight = GoogleFonts.dosisTextTheme(ThemeData.light().textTheme);
 
-final TextTheme textThemeDark = GoogleFonts.playTextTheme(ThemeData.dark().textTheme);
+final TextTheme textThemeDark = GoogleFonts.dosisTextTheme(ThemeData.dark().textTheme);
--- lib/global/theme/themes.dart
+++ lib/global/theme/themes.dart
@@ -6,22 +6,26 @@ import 'texts.dart';
 final accentColor = SystemTheme.accentColor.accent;
 
 ThemeData getLightTheme() => ThemeData(
-      useMaterial3: true,
-      colorScheme: ColorScheme.fromSeed(
-        seedColor: accentColor,
-        // seedColor: Colors.purple,
-        brightness: Brightness.light,
-        // Text
+      useMaterial3: false,
+      colorScheme: const ColorScheme.light(
+        brightness: Brightness.dark,
+        background: Color(0xffefeeee),
+        outline: Color(0xffffffff),
+        outlineVariant: Color(0xffdbdada),
       ),
       textTheme: textThemeLight,
+      iconTheme: const IconThemeData(color: Color(0xff392f41)),
     );
 
 ThemeData getDarkTheme() => ThemeData(
-      useMaterial3: true,
-      colorScheme: ColorScheme.fromSeed(
-        seedColor: accentColor,
+      useMaterial3: false,
+      colorScheme: const ColorScheme.dark(
         brightness: Brightness.dark,
+        background: Color(0xff392f41),
+        outline: Color(0xff4d4355),
+        outlineVariant: Color(0xff251b2d),
       ),
       // Text
       textTheme: textThemeDark,
+      iconTheme: const IconThemeData(color: Color(0xffefeeee)),
     );
