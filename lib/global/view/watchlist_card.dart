23905f0d752bdc0c7e2a0195ef7fd8a3b199748e
Dushan <67400684+DuzziG@users.noreply.github.com>
2024-03-07 10:23:43 +0100

    feat: Add watchlist card

---
+++ lib/global/view/watchlist_card.dart
@@ -0,0 +1,123 @@
+import 'package:flutter/material.dart';
+import 'package:freezed_annotation/freezed_annotation.dart';
+
+import '../ext/context_extensions.dart';
+import '../util/dimens.dart';
+import 'watchlist_tile.dart';
+
+part 'watchlist_card.freezed.dart';
+
+@freezed
+sealed class WatchlistCardTitleData with _$WatchlistCardTitleData {
+  const factory WatchlistCardTitleData({
+    Widget? leading,
+    String? text,
+    String? subtext,
+    String? imageUrl,
+    Widget? trailing,
+  }) = _WatchlistCardTitleData;
+}
+
+@freezed
+sealed class WatchlistCardActionData with _$WatchlistCardActionData {
+  const factory WatchlistCardActionData({
+    String? text,
+    Function? onPressed,
+  }) = _WatchlistCardActionData;
+}
+
+class WatchlistCard extends StatelessWidget {
+  const WatchlistCard({
+    this.padding = const EdgeInsets.all(0),
+    this.color,
+    this.title,
+    this.body,
+    this.actions,
+    this.onTap,
+    this.onLongPress,
+    this.border,
+    this.boxShadow,
+    super.key,
+  });
+
+  final EdgeInsets padding;
+  final Color? color;
+  final WatchlistCardTitleData? title;
+  final Widget? body;
+  final List<WatchlistCardActionData>? actions;
+  final Function? onTap;
+  final Function? onLongPress;
+  final BoxBorder? border;
+  final List<BoxShadow>? boxShadow;
+
+  @override
+  Widget build(context) => Padding(
+        padding: padding,
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            BOX_8,
+            Container(
+              decoration: BoxDecoration(
+                border: border,
+                color: context.colors.secondaryContainer,
+                boxShadow: boxShadow,
+              ),
+              child: Column(
+                children: [
+                  if (title != null) ...[
+                    if (title?.imageUrl != null)
+                      ClipRRect(
+                        borderRadius: const BorderRadius.only(
+                          topLeft: Radius.circular(DOUBLE_16),
+                          topRight: Radius.circular(DOUBLE_16),
+                        ),
+                        child: Image.network(
+                          title?.imageUrl ?? '',
+                          fit: BoxFit.cover,
+                          height: 180,
+                          width: double.infinity,
+                        ),
+                      ),
+                    WatchlistTile(
+                      leading: title?.leading,
+                      title: title?.text != null ? Text(title?.text ?? '') : null,
+                      subtitle: title?.subtext != null
+                          ? Text(
+                              title?.subtext ?? '',
+                              style: context.texts.bodySmall,
+                              maxLines: 2,
+                              overflow: TextOverflow.ellipsis,
+                            )
+                          : null,
+                      trailing: title?.trailing,
+                    ),
+                  ],
+                  if (body != null)
+                    Padding(
+                      padding: INSETS_HORIZONTAL_16.copyWith(
+                        top: title == null ? 16.0 : 0.0,
+                        bottom: actions?.isNotEmpty == true ? 0.0 : 16.0,
+                      ),
+                      child: body,
+                    ),
+                  if (actions != null)
+                    Row(
+                      children: [
+                        ...(actions ?? []).map(
+                          (action) => Expanded(
+                            child: TextButton(
+                              child: Text(action.text ?? ''),
+                              onPressed: () => action.onPressed?.call(),
+                            ),
+                          ),
+                        ),
+                      ],
+                    ),
+                ],
+              ),
+            ),
+          ],
+        ),
+      );
+}
---
+++ lib/global/view/watchlist_card.freezed.dart
@@ -0,0 +1,361 @@
+// coverage:ignore-file
+// GENERATED CODE - DO NOT MODIFY BY HAND
+// ignore_for_file: type=lint
+// ignore_for_file: unused_element, deprecated_member_use, deprecated_member_use_from_same_package, use_function_type_syntax_for_parameters, unnecessary_const, avoid_init_to_null, invalid_override_different_default_values_named, prefer_expression_function_bodies, annotate_overrides, invalid_annotation_target, unnecessary_question_mark
+
+part of 'watchlist_card.dart';
+
+// **************************************************************************
+// FreezedGenerator
+// **************************************************************************
+
+T _$identity<T>(T value) => value;
+
+final _privateConstructorUsedError = UnsupportedError(
+    'It seems like you constructed your class using `MyClass._()`. This constructor is only meant to be used by freezed and you are not supposed to need it nor use it.\nPlease check the documentation here for more information: https://github.com/rrousselGit/freezed#adding-getters-and-methods-to-our-models');
+
+/// @nodoc
+mixin _$WatchlistCardTitleData {
+  Widget? get leading => throw _privateConstructorUsedError;
+  String? get text => throw _privateConstructorUsedError;
+  String? get subtext => throw _privateConstructorUsedError;
+  String? get imageUrl => throw _privateConstructorUsedError;
+  Widget? get trailing => throw _privateConstructorUsedError;
+
+  @JsonKey(ignore: true)
+  $WatchlistCardTitleDataCopyWith<WatchlistCardTitleData> get copyWith =>
+      throw _privateConstructorUsedError;
+}
+
+/// @nodoc
+abstract class $WatchlistCardTitleDataCopyWith<$Res> {
+  factory $WatchlistCardTitleDataCopyWith(WatchlistCardTitleData value,
+          $Res Function(WatchlistCardTitleData) then) =
+      _$WatchlistCardTitleDataCopyWithImpl<$Res, WatchlistCardTitleData>;
+  @useResult
+  $Res call(
+      {Widget? leading,
+      String? text,
+      String? subtext,
+      String? imageUrl,
+      Widget? trailing});
+}
+
+/// @nodoc
+class _$WatchlistCardTitleDataCopyWithImpl<$Res,
+        $Val extends WatchlistCardTitleData>
+    implements $WatchlistCardTitleDataCopyWith<$Res> {
+  _$WatchlistCardTitleDataCopyWithImpl(this._value, this._then);
+
+  // ignore: unused_field
+  final $Val _value;
+  // ignore: unused_field
+  final $Res Function($Val) _then;
+
+  @pragma('vm:prefer-inline')
+  @override
+  $Res call({
+    Object? leading = freezed,
+    Object? text = freezed,
+    Object? subtext = freezed,
+    Object? imageUrl = freezed,
+    Object? trailing = freezed,
+  }) {
+    return _then(_value.copyWith(
+      leading: freezed == leading
+          ? _value.leading
+          : leading // ignore: cast_nullable_to_non_nullable
+              as Widget?,
+      text: freezed == text
+          ? _value.text
+          : text // ignore: cast_nullable_to_non_nullable
+              as String?,
+      subtext: freezed == subtext
+          ? _value.subtext
+          : subtext // ignore: cast_nullable_to_non_nullable
+              as String?,
+      imageUrl: freezed == imageUrl
+          ? _value.imageUrl
+          : imageUrl // ignore: cast_nullable_to_non_nullable
+              as String?,
+      trailing: freezed == trailing
+          ? _value.trailing
+          : trailing // ignore: cast_nullable_to_non_nullable
+              as Widget?,
+    ) as $Val);
+  }
+}
+
+/// @nodoc
+abstract class _$$WatchlistCardTitleDataImplCopyWith<$Res>
+    implements $WatchlistCardTitleDataCopyWith<$Res> {
+  factory _$$WatchlistCardTitleDataImplCopyWith(
+          _$WatchlistCardTitleDataImpl value,
+          $Res Function(_$WatchlistCardTitleDataImpl) then) =
+      __$$WatchlistCardTitleDataImplCopyWithImpl<$Res>;
+  @override
+  @useResult
+  $Res call(
+      {Widget? leading,
+      String? text,
+      String? subtext,
+      String? imageUrl,
+      Widget? trailing});
+}
+
+/// @nodoc
+class __$$WatchlistCardTitleDataImplCopyWithImpl<$Res>
+    extends _$WatchlistCardTitleDataCopyWithImpl<$Res,
+        _$WatchlistCardTitleDataImpl>
+    implements _$$WatchlistCardTitleDataImplCopyWith<$Res> {
+  __$$WatchlistCardTitleDataImplCopyWithImpl(
+      _$WatchlistCardTitleDataImpl _value,
+      $Res Function(_$WatchlistCardTitleDataImpl) _then)
+      : super(_value, _then);
+
+  @pragma('vm:prefer-inline')
+  @override
+  $Res call({
+    Object? leading = freezed,
+    Object? text = freezed,
+    Object? subtext = freezed,
+    Object? imageUrl = freezed,
+    Object? trailing = freezed,
+  }) {
+    return _then(_$WatchlistCardTitleDataImpl(
+      leading: freezed == leading
+          ? _value.leading
+          : leading // ignore: cast_nullable_to_non_nullable
+              as Widget?,
+      text: freezed == text
+          ? _value.text
+          : text // ignore: cast_nullable_to_non_nullable
+              as String?,
+      subtext: freezed == subtext
+          ? _value.subtext
+          : subtext // ignore: cast_nullable_to_non_nullable
+              as String?,
+      imageUrl: freezed == imageUrl
+          ? _value.imageUrl
+          : imageUrl // ignore: cast_nullable_to_non_nullable
+              as String?,
+      trailing: freezed == trailing
+          ? _value.trailing
+          : trailing // ignore: cast_nullable_to_non_nullable
+              as Widget?,
+    ));
+  }
+}
+
+/// @nodoc
+
+class _$WatchlistCardTitleDataImpl implements _WatchlistCardTitleData {
+  const _$WatchlistCardTitleDataImpl(
+      {this.leading, this.text, this.subtext, this.imageUrl, this.trailing});
+
+  @override
+  final Widget? leading;
+  @override
+  final String? text;
+  @override
+  final String? subtext;
+  @override
+  final String? imageUrl;
+  @override
+  final Widget? trailing;
+
+  @override
+  String toString() {
+    return 'WatchlistCardTitleData(leading: $leading, text: $text, subtext: $subtext, imageUrl: $imageUrl, trailing: $trailing)';
+  }
+
+  @override
+  bool operator ==(Object other) {
+    return identical(this, other) ||
+        (other.runtimeType == runtimeType &&
+            other is _$WatchlistCardTitleDataImpl &&
+            (identical(other.leading, leading) || other.leading == leading) &&
+            (identical(other.text, text) || other.text == text) &&
+            (identical(other.subtext, subtext) || other.subtext == subtext) &&
+            (identical(other.imageUrl, imageUrl) ||
+                other.imageUrl == imageUrl) &&
+            (identical(other.trailing, trailing) ||
+                other.trailing == trailing));
+  }
+
+  @override
+  int get hashCode =>
+      Object.hash(runtimeType, leading, text, subtext, imageUrl, trailing);
+
+  @JsonKey(ignore: true)
+  @override
+  @pragma('vm:prefer-inline')
+  _$$WatchlistCardTitleDataImplCopyWith<_$WatchlistCardTitleDataImpl>
+      get copyWith => __$$WatchlistCardTitleDataImplCopyWithImpl<
+          _$WatchlistCardTitleDataImpl>(this, _$identity);
+}
+
+abstract class _WatchlistCardTitleData implements WatchlistCardTitleData {
+  const factory _WatchlistCardTitleData(
+      {final Widget? leading,
+      final String? text,
+      final String? subtext,
+      final String? imageUrl,
+      final Widget? trailing}) = _$WatchlistCardTitleDataImpl;
+
+  @override
+  Widget? get leading;
+  @override
+  String? get text;
+  @override
+  String? get subtext;
+  @override
+  String? get imageUrl;
+  @override
+  Widget? get trailing;
+  @override
+  @JsonKey(ignore: true)
+  _$$WatchlistCardTitleDataImplCopyWith<_$WatchlistCardTitleDataImpl>
+      get copyWith => throw _privateConstructorUsedError;
+}
+
+/// @nodoc
+mixin _$WatchlistCardActionData {
+  String? get text => throw _privateConstructorUsedError;
+  Function? get onPressed => throw _privateConstructorUsedError;
+
+  @JsonKey(ignore: true)
+  $WatchlistCardActionDataCopyWith<WatchlistCardActionData> get copyWith =>
+      throw _privateConstructorUsedError;
+}
+
+/// @nodoc
+abstract class $WatchlistCardActionDataCopyWith<$Res> {
+  factory $WatchlistCardActionDataCopyWith(WatchlistCardActionData value,
+          $Res Function(WatchlistCardActionData) then) =
+      _$WatchlistCardActionDataCopyWithImpl<$Res, WatchlistCardActionData>;
+  @useResult
+  $Res call({String? text, Function? onPressed});
+}
+
+/// @nodoc
+class _$WatchlistCardActionDataCopyWithImpl<$Res,
+        $Val extends WatchlistCardActionData>
+    implements $WatchlistCardActionDataCopyWith<$Res> {
+  _$WatchlistCardActionDataCopyWithImpl(this._value, this._then);
+
+  // ignore: unused_field
+  final $Val _value;
+  // ignore: unused_field
+  final $Res Function($Val) _then;
+
+  @pragma('vm:prefer-inline')
+  @override
+  $Res call({
+    Object? text = freezed,
+    Object? onPressed = freezed,
+  }) {
+    return _then(_value.copyWith(
+      text: freezed == text
+          ? _value.text
+          : text // ignore: cast_nullable_to_non_nullable
+              as String?,
+      onPressed: freezed == onPressed
+          ? _value.onPressed
+          : onPressed // ignore: cast_nullable_to_non_nullable
+              as Function?,
+    ) as $Val);
+  }
+}
+
+/// @nodoc
+abstract class _$$WatchlistCardActionDataImplCopyWith<$Res>
+    implements $WatchlistCardActionDataCopyWith<$Res> {
+  factory _$$WatchlistCardActionDataImplCopyWith(
+          _$WatchlistCardActionDataImpl value,
+          $Res Function(_$WatchlistCardActionDataImpl) then) =
+      __$$WatchlistCardActionDataImplCopyWithImpl<$Res>;
+  @override
+  @useResult
+  $Res call({String? text, Function? onPressed});
+}
+
+/// @nodoc
+class __$$WatchlistCardActionDataImplCopyWithImpl<$Res>
+    extends _$WatchlistCardActionDataCopyWithImpl<$Res,
+        _$WatchlistCardActionDataImpl>
+    implements _$$WatchlistCardActionDataImplCopyWith<$Res> {
+  __$$WatchlistCardActionDataImplCopyWithImpl(
+      _$WatchlistCardActionDataImpl _value,
+      $Res Function(_$WatchlistCardActionDataImpl) _then)
+      : super(_value, _then);
+
+  @pragma('vm:prefer-inline')
+  @override
+  $Res call({
+    Object? text = freezed,
+    Object? onPressed = freezed,
+  }) {
+    return _then(_$WatchlistCardActionDataImpl(
+      text: freezed == text
+          ? _value.text
+          : text // ignore: cast_nullable_to_non_nullable
+              as String?,
+      onPressed: freezed == onPressed
+          ? _value.onPressed
+          : onPressed // ignore: cast_nullable_to_non_nullable
+              as Function?,
+    ));
+  }
+}
+
+/// @nodoc
+
+class _$WatchlistCardActionDataImpl implements _WatchlistCardActionData {
+  const _$WatchlistCardActionDataImpl({this.text, this.onPressed});
+
+  @override
+  final String? text;
+  @override
+  final Function? onPressed;
+
+  @override
+  String toString() {
+    return 'WatchlistCardActionData(text: $text, onPressed: $onPressed)';
+  }
+
+  @override
+  bool operator ==(Object other) {
+    return identical(this, other) ||
+        (other.runtimeType == runtimeType &&
+            other is _$WatchlistCardActionDataImpl &&
+            (identical(other.text, text) || other.text == text) &&
+            (identical(other.onPressed, onPressed) ||
+                other.onPressed == onPressed));
+  }
+
+  @override
+  int get hashCode => Object.hash(runtimeType, text, onPressed);
+
+  @JsonKey(ignore: true)
+  @override
+  @pragma('vm:prefer-inline')
+  _$$WatchlistCardActionDataImplCopyWith<_$WatchlistCardActionDataImpl>
+      get copyWith => __$$WatchlistCardActionDataImplCopyWithImpl<
+          _$WatchlistCardActionDataImpl>(this, _$identity);
+}
+
+abstract class _WatchlistCardActionData implements WatchlistCardActionData {
+  const factory _WatchlistCardActionData(
+      {final String? text,
+      final Function? onPressed}) = _$WatchlistCardActionDataImpl;
+
+  @override
+  String? get text;
+  @override
+  Function? get onPressed;
+  @override
+  @JsonKey(ignore: true)
+  _$$WatchlistCardActionDataImplCopyWith<_$WatchlistCardActionDataImpl>
+      get copyWith => throw _privateConstructorUsedError;
+}
