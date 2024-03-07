b16d9fa14ec5199c5a92c1d7588c1890dd604a66
Dushan <67400684+DuzziG@users.noreply.github.com>
2024-03-07 10:25:32 +0100

    feat: Add add_ticker_dialog

---
+++ lib/watchlist/add_ticker_dialog.dart
@@ -0,0 +1,55 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:stock_watchlist/global/ext/context_extensions.dart';
+import 'package:stock_watchlist/global/util/dimens.dart';
+import 'package:stock_watchlist/global/view/neumorphic_wrapper.dart';
+import 'package:stock_watchlist/global/view/reactive_text_field.dart';
+import 'package:stock_watchlist/watchlist/watchlist_controller.dart';
+
+class AddTickerDialog extends ConsumerWidget {
+  const AddTickerDialog({super.key});
+
+  @override
+  Widget build(context, ref) {
+    final state = ref.watch(watchlistControllerProvider);
+
+    return Material(
+      color: context.colors.background,
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          NeumorphicWrapper(
+            height: 60,
+            width: 100,
+            child: ReactiveTextField(
+              text: state.tempTicker,
+              textAlign: TextAlign.center,
+              autofocus: true,
+              autocorrect: true,
+              maxLines: 1,
+              minLines: 1,
+              textCapitalization: TextCapitalization.characters,
+              keyboardType: TextInputType.multiline,
+              onChange: (value) => ref.controller().onEvent(InputChanged(value)),
+            ),
+          ),
+          BOX_16,
+          NeumorphicWrapper(
+            height: 50,
+            child: IconButton(
+              style: ButtonStyle(
+                backgroundColor: MaterialStateProperty.all(context.colors.background),
+              ),
+              icon: const Icon(Icons.send),
+              onPressed: () => ref.controller().onEvent(Submit(state.tempTicker)),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+extension _AddTickerControllerExt on WidgetRef {
+  WatchlistController controller() => read(watchlistControllerProvider.notifier);
+}
