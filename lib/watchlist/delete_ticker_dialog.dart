cb1a3fe49ca5fe98b0e4859ce0db7c33398be26c
Dushan <67400684+DuzziG@users.noreply.github.com>
2024-03-08 11:59:24 +0100

    chore: Improve and rename delete_ticker_dialog

---
+++ lib/watchlist/delete_ticker_dialog.dart
@@ -0,0 +1,62 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:stock_watchlist/global/ext/context_extensions.dart';
+import 'package:stock_watchlist/global/util/dimens.dart';
+import 'package:stock_watchlist/watchlist/watchlist_controller.dart';
+
+class DeleteTickerDialog extends ConsumerWidget {
+  const DeleteTickerDialog(String? ticker, {super.key});
+
+  @override
+  Widget build(context, ref) {
+    final state = ref.watch(watchlistControllerProvider);
+
+    return Dialog(
+      backgroundColor: Colors.transparent,
+      child: IntrinsicHeight(
+        child: Container(
+          decoration: BoxDecoration(
+            color: context.colors.background,
+            borderRadius: BorderRadius.circular(DOUBLE_16),
+          ),
+          child: Padding(
+            padding: INSETS_32,
+            child: Column(
+              children: [
+                const Text('Are you sure?'),
+                BOX_16,
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceAround,
+                  children: [
+                    TextButton.icon(
+                      style: ButtonStyle(
+                        backgroundColor: MaterialStateProperty.all(context.colors.background),
+                        foregroundColor: MaterialStateProperty.all(context.colors.errorContainer),
+                      ),
+                      icon: const Icon(Icons.delete_outline),
+                      onPressed: () => ref.controller().onEvent(DeleteTicker(state.ticker ?? '')),
+                      label: const Text('Delete'),
+                    ),
+                    BOX_16,
+                    TextButton.icon(
+                      style: ButtonStyle(
+                        backgroundColor: MaterialStateProperty.all(context.colors.background),
+                      ),
+                      icon: const Icon(Icons.cancel_outlined),
+                      onPressed: () => ref.controller().onEvent(const DismissDialog()),
+                      label: const Text('Cancel'),
+                    ),
+                  ],
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+extension _AddTickerControllerExt on WidgetRef {
+  WatchlistController controller() => read(watchlistControllerProvider.notifier);
+}
