77733b28ee8ccf24e55a598fb18dd583fe7b82c9
Dushan <67400684+DuzziG@users.noreply.github.com>
2024-03-13 09:04:55 +0100

    chore: Switch ticker_data provider to the alpaca repo.

--- lib/global/di/ticker_data_providers.dart
+++ lib/global/di/ticker_data_providers.dart
@@ -1,28 +1,8 @@
-import 'package:awesome_dio_interceptor/awesome_dio_interceptor.dart';
-import 'package:dio/dio.dart';
-import 'package:flutter/material.dart';
 import 'package:riverpod_annotation/riverpod_annotation.dart';
 
-import '../../data/repository/ticker_data_repository.dart';
-import '../util/consts.dart';
+import '../../data/repository/crypto_data_repository.dart';
 
 part 'ticker_data_providers.g.dart';
 
 @Riverpod(keepAlive: true)
-Dio dio(DioRef ref) => Dio()
-  ..interceptors.addAll([
-    InterceptorsWrapper(
-      onRequest: (options, handler) async {
-        options.baseUrl = BASE_URL;
-        return handler.next(options);
-      },
-      onResponse: (response, handler) => handler.next(response),
-      onError: (error, handler) => handler.next(error),
-    ),
-    AwesomeDioInterceptor(logger: debugPrint),
-  ]);
-
-@Riverpod(keepAlive: true)
-TickerDataRepository tickerDataRepository(TickerDataRepositoryRef ref) => AlphaVantageTickerDataRepository(
-      dio: ref.watch(dioProvider),
-    );
+CryptoDataRepository cryptoDataRepository(CryptoDataRepositoryRef ref) => AlpacaCryptoDataRepository();
--- lib/global/di/ticker_data_providers.g.dart
+++ lib/global/di/ticker_data_providers.g.dart
@@ -6,35 +6,21 @@ part of 'ticker_data_providers.dart';
 // RiverpodGenerator
 // **************************************************************************
 
-String _$dioHash() => r'3afaba362472fdb61bd8fd3cd499949df579dd62';
+String _$cryptoDataRepositoryHash() =>
+    r'bd25aec2df963237a35659d849dce447df3e5b75';
 
-/// See also [dio].
-@ProviderFor(dio)
-final dioProvider = Provider<Dio>.internal(
-  dio,
-  name: r'dioProvider',
-  debugGetCreateSourceHash:
-      const bool.fromEnvironment('dart.vm.product') ? null : _$dioHash,
-  dependencies: null,
-  allTransitiveDependencies: null,
-);
-
-typedef DioRef = ProviderRef<Dio>;
-String _$tickerDataRepositoryHash() =>
-    r'a590d1a431fa28067f3194b2c0155a4221acd528';
-
-/// See also [tickerDataRepository].
-@ProviderFor(tickerDataRepository)
-final tickerDataRepositoryProvider = Provider<TickerDataRepository>.internal(
-  tickerDataRepository,
-  name: r'tickerDataRepositoryProvider',
+/// See also [cryptoDataRepository].
+@ProviderFor(cryptoDataRepository)
+final cryptoDataRepositoryProvider = Provider<CryptoDataRepository>.internal(
+  cryptoDataRepository,
+  name: r'cryptoDataRepositoryProvider',
   debugGetCreateSourceHash: const bool.fromEnvironment('dart.vm.product')
       ? null
-      : _$tickerDataRepositoryHash,
+      : _$cryptoDataRepositoryHash,
   dependencies: null,
   allTransitiveDependencies: null,
 );
 
-typedef TickerDataRepositoryRef = ProviderRef<TickerDataRepository>;
+typedef CryptoDataRepositoryRef = ProviderRef<CryptoDataRepository>;
 // ignore_for_file: type=lint
 // ignore_for_file: subtype_of_sealed_class, invalid_use_of_internal_member, invalid_use_of_visible_for_testing_member, inference_failure_on_uninitialized_variable, inference_failure_on_function_return_type, inference_failure_on_untyped_parameter, deprecated_member_use_from_same_package
