3367552b63a731a589627d659c8c5cfc74c71596
Dushan <67400684+DuzziG@users.noreply.github.com>
2024-03-13 09:16:17 +0100

    chore: Add alpaca urls keys and secret to env and consts

--- lib/global/util/consts.dart
+++ lib/global/util/consts.dart
@@ -1,6 +1,14 @@
 // ignore_for_file: constant_identifier_names
 
 const BASE_URL = 'https://www.alphavantage.co';
+const ALPACA_URL = 'wss://stream.data.alpaca.markets/v2/iex';
+const ALPACA_CRYPTO_URL = 'wss://stream.data.alpaca.markets/v1beta3/crypto/us';
+const ALPACA_SANDBOX_URL = 'wss://stream.data.sandbox.alpaca.markets/v2/iex';
+
+const ACTION_TYPE_TRADES = "trades";
+const ACTION_TYPE_QUOTES = "quotes";
+const ACTION_TYPE_BARS = "bars";
+const ACTION_TYPE_ORDERBOOKS = "orderbooks";
 
 const KEY_AUTHENTICATION = 'key_authentication';
 const KEY_THEME_MODE = 'theme_mode';
--- lib/global/util/env.dart
+++ lib/global/util/env.dart
@@ -9,4 +9,16 @@ abstract class Env {
     obfuscate: true,
   )
   static String apikey = _Env.apikey;
+
+  @EnviedField(
+    varName: 'KEY_ALPACA_KEY',
+    obfuscate: true,
+  )
+  static String alpacakey = _Env.alpacakey;
+
+  @EnviedField(
+    varName: 'KEY_ALPACA_SECRET',
+    obfuscate: true,
+  )
+  static String alpacasecret = _Env.alpacasecret;
 }
